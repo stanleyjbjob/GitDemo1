e1f77b16ce89f89121e162b2b19ff8147dbc7089
JBJOB\stanley <stanley@jbjob.com.tw>
2023-08-18 14:43:16 +0800

    Test1

---
+++ Test1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+	public class Test1
+	{
+		public void SayHi()
+		{
+			Console.WriteLine("Hi World");
+		}
+	}
+}
