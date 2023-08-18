3e9848876945e2c81881ca220c778aeef5fb5e11
JBJOB\stanley <stanley@jbjob.com.tw>
2023-08-18 14:54:31 +0800

    fix merge

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GitDemo;
 
-Console.WriteLine("Hello, 傑報!");
+Console.WriteLine("你好, 傑報!");
 Test1 test1=new Test1();
 test1.SayHi();
-Console.WriteLine("你好, 傑報!");
